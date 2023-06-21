71e0d013f6cedb42e11f113ba9a6c7107bf13b9e
Ayush1M <worklife2442@gmail.com>
2023-06-21 21:34:22 +0530

    added handleclick function to select an answer and updated styling to chose right or wrong answer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,44 +1,69 @@
 import React from "react";
 import decode from "../assets/decode";
+import classNames from "classnames";
 
+export default function Question(props) {
+    
+  function handleClick(id, selectedAnswer) {
+    props.setQuestions((prevQuestion) => {
+      return prevQuestion.map((question) => {
+        if (question.id === id) {
+          return { ...question, selectedAnswer };
+        } else {
+          return question;
+        }
+      });
+    });
+  }
 
-export default function Question(props){
+  const answerElement = props.allAnswers.map((ans) => {
+    return (
+      <>
+        <input
+          
+          className="answer-item"
+          type="radio"
+          name={props.questionId}
+          id={ans.answerId}
+          value={ans.answer}
+          onChange={() => handleClick(props.questionId, ans.answer)}
+        />
+        <label
+          className={classNames(
+            "answer-label",
+            {
+              "answer-label--selected":
+              props.GameOver && ans.answer === props.selectedAnswer,
+            },
+            {
+              "answer-label--correct":
+              !props.GameOver && ans.answer === props.correctAnswer,
+            },
+            {
+              "answer-label--incorrect":
+              !props.GameOver && ans.answer !== props.correctAnswer,
+            },
+            {
+              "answer-label--unselected":
+                !props.GameOver &&
+                ans.answer !== props.correctAnswer &&
+                ans.answer !== props.selectedAnswer,
+            }
+          )}
+          htmlFor={ans.answerId}
+        >
+          {ans.answer}
+        </label>
+      </>
+    );
+  });
 
-
-    function handleClick(id, selectedAnswer){
-        props.setQuestions(prevQuestion => {
-            return prevQuestion.map(question => {
-                if(question.id === id){
-                    return {...question,
-                        selectedAnswer}
-                }else{
-                  return question
-                }           
-            })
-        })
-    }
-
-
-    const answerElement = props.allAnswers.map((ans) => {
-
-        return (
-            <>
-            <input 
-            className="hidden"
-            type="radio"
-            name={props.questionId}
-            id={ans.answerId}
-            value={ans.answer}
-            onChange={() => handleClick(props.questionId, ans.answer)}/>
-            <label className={`text-lg rounded border-2 mr-4 px-4 ${props.selectedAnswer === ans.answer ? "bg-light-gray" : ""}`} htmlFor={ans.answerId}>{ans.answer}</label>
-            </>
-        )
-    })
-
-    return(
-       <>
-       <h2>{decode(props.question)}</h2>
-       <div>{answerElement}</div>
-       </>
-    )
+  return (
+    <>
+    <div>
+      <h2 className="question-list">{decode(props.question)}</h2>
+      <div className="answer-container">{answerElement}</div>
+      </div>
+    </>
+  );
 }
