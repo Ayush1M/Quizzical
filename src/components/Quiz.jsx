a24e352fa8a86a1a46fab3b7dec05928b39f8801
Ayush1M <worklife2442@gmail.com>
2023-06-23 01:18:31 +0530

    Added reset button to reset the game.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,110 +1,113 @@
 import React, { useEffect, useState } from "react";
 import decode from "../assets/decode";
-import { nanoid } from 'nanoid'
+import { nanoid } from "nanoid";
 import Question from "./Question";
 
+export default function Quiz() {
+  // to show questions from API
+  const [questions, setQuestions] = useState([]);
+  // state to manage after the game is over
+  const [gameOver, setIsGameOver] = useState(true);
+  // reset the game
+  const [reset, setReset] = useState(false);
+  let score = 0;
 
-export default function Quiz(){
-    const [questions, setQuestions] = useState([])
-    const [gameOver, setIsGameOver] = useState(true)
-    let score = 0
+  // to check each question for correct answer and update the score
+  questions.forEach((q) => {
+    q.selectedAnswer === q.correctAnswer ? (score = score + 1) : score;
+  });
 
-    questions.forEach((q) => {
-        q.selectedAnswer === q.correctAnswer ? score = score + 1 : score
-    })
-    
+  // disable button to see if all the answers have been selected
+  let disableBtn = questions.every((question) => question.selectedAnswer);
 
-    let disableBtn = questions.every((question) => question.selectedAnswer)
-
-    function checkAnswers(){
-        setIsGameOver(prevState => !prevState)
-    }
-
-    function resetGame(){
-        setQuestions(questions)
-    }
-    
+  // checking the answers when the game is over
+  function checkAnswers() {
+    setIsGameOver((prevState) => !prevState);
+  }
 
   useEffect(() => {
+    async function getQuiz() {
+      const res = await fetch(
+        "https://opentdb.com/api.php?amount=5&category=21&difficulty=medium&type=multiple"
+      );
+      const data = await res.json();
 
-    async function getQuiz(){
-        const res = await fetch("https://opentdb.com/api.php?amount=5&category=21&difficulty=medium&type=multiple")
-        const data = await res.json()
-            
-            const updatedQuestions = data.results.map((question)=> {
-            const questionn = question.question
+      setQuestions(
+        data.results.map((question) => {
+          const questionn = question.question;
 
-            const correctAnswer = {
-                id: nanoid(),
-                answer : question.correct_answer
-            } 
+          const correctAnswer = {
+            id: nanoid(),
+            answer: question.correct_answer,
+          };
 
-            const incorrectAnswer = question.incorrect_answers.map((incorrect) => ({
-                id : nanoid(),
-                answer : incorrect
-            }))
+          let allAnswers = [];
+          allAnswers.push(...question.incorrect_answers);
+          allAnswers.push(question.correct_answer);
+          allAnswers.sort(() => Math.random() - 0.5);
+          allAnswers = allAnswers.map((ans) => ({
+            answerId: nanoid(),
+            answer: decode(ans),
+          }));
 
-            let allAnswers = []
-            allAnswers.push(...question.incorrect_answers)
-            allAnswers.push(question.correct_answer)
-            allAnswers.sort(()=> Math.random() - 0.5)
-            allAnswers = allAnswers.map((ans) => ({
-                answerId: nanoid(),
-                answer : decode(ans),
-            }))
-
-            return{
-                ...question,
-                id : nanoid(),
-                question : questionn,
-                allAnswers : allAnswers,
-                selectedAnswer : null,
-                correctAnswer : correctAnswer.answer,
-                incorrectAnswer: incorrectAnswer
-            }
+          return {
+            ...question,
+            id: nanoid(),
+            question: questionn,
+            allAnswers: allAnswers,
+            selectedAnswer: null,
+            correctAnswer: correctAnswer.answer,
+          };
         })
-        setQuestions(() => updatedQuestions)
+      );
     }
-    console.log(questions);
-   getQuiz()
-  }, [])
+    getQuiz();
+  }, [reset]);
 
+  // reset the game
+  function resetGame() {
+    setReset((prevState) => !prevState);
+    setIsGameOver(true);
+  }
 
   const quizElement = questions.map((q) => (
-        <Question 
-        key = {q.id}
-        questionId = {q.id}
-        question = {q.question}
-        correctAnswer = {q.correctAnswer}
-        incorrectAnswerId = {q.incorrectAnswerId}
-        allAnswers = {q.allAnswers}
-        selectedAnswer = {q.selectedAnswer}
-        questions = {questions}
-        setQuestions = {setQuestions}
-        GameOver = {gameOver}
-        />
-  ))
+    <Question
+      key={q.id}
+      questionId={q.id}
+      question={q.question}
+      correctAnswer={q.correctAnswer}
+      allAnswers={q.allAnswers}
+      selectedAnswer={q.selectedAnswer}
+      setQuestions={setQuestions}
+      GameOver={gameOver}
+    />
+  ));
 
-
-    return(
-       <>
-       <div className="quiz-container">
-        <div className="question-container">
-          {quizElement}
-          </div>
-          {gameOver && <button 
-              disabled={!disableBtn} 
-              className="check-answers-btn"
-              onClick={checkAnswers}
-              type="submit"
-            >
-                {disableBtn ? 
-                "Check Answers" : 
-                "Select your Answers"}
-            </button>}
-            {!gameOver && <h2 className="score">You Scored {score} / {questions.length}</h2>}
-            {!gameOver && <button className="reset-btn" onClick={resetGame}>{score === questions.length ? "New Game" : "Play Again"}</button>}
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="quiz-container">
+        <div className="question-container">{quizElement}</div>
+        {gameOver && (
+          <button
+            disabled={!disableBtn}
+            className="check-answers-btn"
+            onClick={checkAnswers}
+            type="submit"
+          >
+            {disableBtn ? "Check Answers" : "Select your Answers"}
+          </button>
+        )}
+        {!gameOver && (
+          <h2 className="score">
+            You Scored {score} / {questions.length}
+          </h2>
+        )}
+        {!gameOver && (
+          <button className="reset-btn" onClick={resetGame}>
+            {score === questions.length ? "New Game😄" : "Play Again😟"}
+          </button>
+        )}
+      </div>
+    </>
+  );
 }
